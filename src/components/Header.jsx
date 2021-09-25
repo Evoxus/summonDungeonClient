ab8a55154e65cf3929c0469f5ca7be923aef26cd
Michael Ploughman <michaelhploughman@gmail.com>
2021-09-25 12:40:39 -0600

    Add styling and bootstrap cdn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
 export default function Header() {
   return (
-    <header className="header">
-      <h1>summonDungeon</h1>
-    </header>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">summonDungeon</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Characters</Nav.Link>
+            <Nav.Link href="#link">New Character</Nav.Link>
+            <Nav.Link href="#link">Dice Roller</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
